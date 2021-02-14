d1cf5ef5e8d401a7c8b847e57b9e857faed1b970
Lucas Oliveira <loliveira.development@gmail.com>
2021-02-13 23:22:57 -0300

    init project helpdesk-v2

---
+++ helpdesk/src/main/java/com/lucasoliveira/helpdesk/HelpdeskV2Application.java
@@ -0,0 +1,13 @@
+package com.lucasoliveira.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskV2Application.class, args);
+	}
+
+}
---
+++ helpdesk/src/test/java/com/lucasoliveira/helpdesk/HelpdeskV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasoliveira.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
